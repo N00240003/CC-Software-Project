e21e105a6aa769abb44c3ec61a201532a2dba255
Ciara <N00240003@student.iadt.ie>
2026-03-04 23:08:02 +0000

    Bat script not work :(

---
+++ My-Project-2026/public/stories/conflict-fighter/compile.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\ciara\CC-Software-Project\My-Project-2026\public\stories\conflict-fighter"
+c:\Users\ciara\CC-Software-Project\tweego-2.1.1-windows-x64\tweego.exe -o index.html chapters main media --watch
