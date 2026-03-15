d08b2ad115e393fa7a161a4d9f65509742d37048
Ciara <N00240003@student.iadt.ie>
2026-03-15 23:29:52 +0000

    Changed compile.bat

--- My-Project-2026/public/stories/conflict-fighter/compile.bat
+++ My-Project-2026/public/stories/conflict-fighter/compile.bat
@@ -1,3 +1,1 @@
-@echo off
-cd /d "C:\Users\ciara\CC-Software-Project\My-Project-2026\public\stories\conflict-fighter"
-c:\Users\ciara\CC-Software-Project\tweego-2.1.1-windows-x64\tweego.exe -o index.html chapters main media --watch
+tweego -f sugarcube-2 -o output/index.html main/main.twee chapters/ --watch
